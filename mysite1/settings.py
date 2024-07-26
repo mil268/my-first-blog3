4ee26b25e440b9108e0c8727c5821fd59a8a12ef
Milton <msuito2002@yahoo.es>
2024-07-25 20:58:56 -0500

    Asegurar el sitio, se crea vista y plantilla para el usuario pueda registrarse.

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "/"
+
