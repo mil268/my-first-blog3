476a968ec46dd71f944c784fd427a272cd0e18dd
Milton <msuito2002@yahoo.es>
2024-08-13 14:03:48 -0500

    agregado ckeditor,cambios en base.html,blog.css,models.py,post_edit.html y settings.py.

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = RichTextField(blank=True, null=True)
+    text = RichTextField(blank=True,null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
